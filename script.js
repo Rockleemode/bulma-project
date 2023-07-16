540b14671bbced565d230ddf35e0592b6e3bc840
Rockleemode <96262404+Rockleemode@users.noreply.github.com>
2023-07-16 16:58:15 +0100

    included the tabs

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-    const nav = document.querySelector("#nav-links");
-    const icon = document.querySelector("#burger-icon");
+const nav = document.querySelector("#nav-links");
+const icon = document.querySelector("#burger-icon");
 
+icon.addEventListener("click", () => {
+  nav.classList.toggle("is-active");
+});
 
-    icon.addEventListener("click", () =>{
-        nav.classList.toggle("is-active");
-    });         
+const tabs = document.querySelectorAll(".tabs li");
+const content = document.querySelectorAll("#tab-content > div");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    tabs.forEach((item) => {
+      item.classList.remove("is-active");
+    });
+    tab.classList.add("is-active");
+
+    const target = tab.dataset.target;
+
+    content.forEach((bro) => {
+      if (bro.getAttribute("id") == target) {
+        bro.classList.remove("is-hidden");
+      } else bro.classList.add("is-hidden");
+    });
+  });
+});
